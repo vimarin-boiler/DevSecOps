94af2bee8cfb7f63cbba7c10aabfc21855fd8d28
Mauricio Marín T. <vimarin@boiler.cl>
2025-10-16 16:10:26 -0300

    la suma ahora suma

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +11,11 @@ public class HelloController {
   public String hello() {
     return "Hola desde Spring Boot para DevSecOps!";
   }
-  
+
   @GetMapping("/suma")
-  public int suma(){
-    return 1;
+  public int suma(@RequestParam(name = "a", required = true) int a,
+                  @RequestParam(name = "b", required = true) int b){
+    return a + b;
   }
 }
 
